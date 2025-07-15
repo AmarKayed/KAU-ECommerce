1ce60a054442e4be6e0f6bffc60ba3381e98c571
Amar Kayed <amar.kayed18@gmail.com>
2025-07-15 13:58:13 +0300

    Add Product And Category Models

---
+++ ECommerce.DAL/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ECommerce.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        // Define DbSets for your entities here
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ ECommerce.DAL/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommerce.DAL.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public Guid CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
