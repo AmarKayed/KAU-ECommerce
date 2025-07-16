75875816ac330700ef247501a613a4bc0ac4946e
Amar Kayed <amar.kayed18@gmail.com>
2025-07-16 10:35:48 +0300

    Add Product Model Annotations

--- ECommerce.DAL/Models/Product.cs
+++ ECommerce.DAL/Models/Product.cs
@@ -1,10 +1,21 @@
-﻿namespace ECommerce.DAL.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECommerce.DAL.Models
 {
+    [Table("ecommerce_products")]
     public class Product
     {
+        [Key]
         public Guid Id { get; set; }
+
+        [MaxLength(255)]
+        [Column("product_name")]
         public string Name { get; set; }
+        
         public decimal Price { get; set; }
+
+        [NotMapped]
         public string Description { get; set; }
         public Guid CategoryId { get; set; }
         public Category Category { get; set; }
