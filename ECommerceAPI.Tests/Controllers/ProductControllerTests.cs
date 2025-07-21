514a7a552364fca2285f1dcf025e15e8c6122ae8
Amar Kayed <amar.kayed18@gmail.com>
2025-07-21 13:38:22 +0300

    Add ECommerceAPI.Tests Project

---
+++ ECommerceAPI.Tests/Controllers/ProductControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using ECommerce.DAL.Interfaces;
+using ECommerce.DAL.Models;
+using ECommerceAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace ECommerceAPI.Tests.Controllers
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public async Task GetProductsByName_ReturnsOkResult_WithProducts()
+        {
+            // Arrange
+            var mockRepo = new Mock<IProductRepository>();
+            mockRepo.Setup(repo => repo.GetProductsByNameAsync("Test"))
+                    .ReturnsAsync(new List<Product> { new Product { Name = "Test" } });
+
+            var controller = new ProductController(mockRepo.Object);
+
+            // Act
+            var result = await controller.GetProductsByName("Test");
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var products = Assert.IsAssignableFrom<IEnumerable<Product>>(okResult.Value);
+            Assert.Single(products);
+            Assert.Contains(products, p => p.Name == "Test");
+        }
+    }
+}
