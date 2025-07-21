3c37e2dd064ba4568ad67f0916876ae328316d32
Amar Kayed <amar.kayed18@gmail.com>
2025-07-21 13:53:40 +0300

    Add ECommerce.Web.Tests Project

---
+++ ECommerce.Web.Tests/ProductControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using ECommerce.DAL.Interfaces;
+using ECommerce.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace ECommerce.Web.Tests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public async Task Index_ReturnsViewResult()
+        {
+            // Arrange
+            var mockRepo = new Mock<IProductRepository>();
+            var controller = new ProductController(mockRepo.Object);
+
+            // Act
+            var result = await controller.Index();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Null(viewResult.ViewName); // Default view
+        }
+    }
+}
